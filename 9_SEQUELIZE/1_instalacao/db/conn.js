307e62651a08ad551c087c2d97c55f8e93d24742
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-05-27 16:59:03 -0300

    .

---
+++ 9_SEQUELIZE/1_instalacao/db/conn.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('sequelize', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3308,
+});
+
+module.exports = sequelize
