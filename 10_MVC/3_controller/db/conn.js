a0a9ab37656cd92345bebf7ae9a7329a57bb76ff
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-06-02 18:05:50 -0300

    .

---
+++ 10_MVC/3_controller/db/conn.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize')
+
+
+const sequelize = new Sequelize('sequelize_mvc', 'root', 'root', {
+    host: 'localhost',
+    port: 3308,
+    dialect: 'mysql',
+})
+
+
+try {
+    sequelize.authenticate()
+    console.log('Conectado ao db')
+} catch (error) {
+    console.log(`Não foi possível conectar: ${error}`)
+}
+
+module.exports = sequelize
