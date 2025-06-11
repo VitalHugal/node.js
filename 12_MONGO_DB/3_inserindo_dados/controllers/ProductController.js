8f1338468cf9fe590300edf5a17650e44dc5184d
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-06-11 16:04:45 -0300

    .

---
+++ 12_MONGO_DB/3_inserindo_dados/controllers/ProductController.js
@@ -0,0 +1,27 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController {
+    static async showProducts(req, res) {
+        res.render('products/all')
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static async createProductPost(req, res) {
+        try {
+            const name = req.body.name
+            const price = req.body.price
+            const description = req.body.description
+
+            const product = new Product(name, price, description)
+
+            product.save()
+
+            res.redirect('/products')
+        } catch (error) {
+            
+        }
+    }
+}
---
+++ 12_MONGO_DB/4_resgatando_dados/controllers/ProductController.js
@@ -0,0 +1,37 @@
+const Product = require('../models/Product')
+
+module.exports = class ProductController {
+
+    static async showProducts(req, res) {
+        try {
+            const products = await Product.getProducts()
+            console.log(products) 
+            res.render('products/all', { products })
+        } catch (error) {
+            console.error(error)
+            res.status(500).send('Erro ao buscar produtos')
+        }
+    }
+
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static async createProductPost(req, res) {
+        try {
+            const name = req.body.name
+            const image = req.body.image
+            const price = req.body.price
+            const description = req.body.description
+
+            const product = new Product(name, image, price, description)
+
+            product.save()
+
+            res.redirect('/products')
+        } catch (error) {
+
+        }
+    }
+}
