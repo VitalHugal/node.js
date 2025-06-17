5599cd77c8e479b990b9ba88cb4ea9edb0d7017f
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2025-06-17 08:55:42 -0300

    .

--- 14_APIs/3_status/index.js
+++ 14_APIs/3_status/index.js
@@ -14,13 +14,17 @@ app.post('/createproduct', (req, res) => {
     const name = req.body.name
     const price = req.body.price
 
+    if (!name) {
+       return res.status(422).json({message: "campo name é obrigatorio"})
+    }
+
     console.log(name)
     console.log(price)
-    return res.json({ success: true, message: "ok" })
+    return res.status(201).json({ success: true, message: "ok" })
 })
 
 app.get('/', (req, res) => {
-    res.json({ message: "Primeira api" })
+    res.status(200).json({ message: "Primeira api" })
 })
 
 app.listen(3000)
